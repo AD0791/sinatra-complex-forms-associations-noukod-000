58044a556e6e2bbb61aaa6fee7d3db703c9cec15
Alexandro Disla <alexandrodisla@hotmail.com>
2019-08-07 17:47:58 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,41 +1,51 @@
 class PetsController < ApplicationController
 
+  # Display what is in the index file from views/pets folders.
   get '/pets' do
     @pets = Pet.all
     erb :'/pets/index'
   end
 
+ # Display a new form to fill out.
   get '/pets/new' do
-     @owners = Owner.all
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
+  # Add and save the information from the form to the database
   post '/pets' do
+    # binding.pry
     @pet = Pet.create(params[:pet])
-    if !params["owner"]["name"].empty?
-      @pet.owner = Owner.create(name: params["owner"]["name"])
+    if !params[:owner][:name].empty?
+      @pet.owner = Owner.create(params[:owner])
+      @pet.save
     end
-    @pet.save
-    redirect "pets/#{@pet.id}"
+
+    redirect to "pets/#{@pet.id}"
   end
 
+  # Display a form to modify by taking it from the database
   get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
-  #patch
+
+  # Display a form to modify a previous Record
+    get '/pets/:id/edit' do
+      @pet = Pet.find(params[:id])
+      @owners = Owner.all
+      erb :'/pets/edit'
+    end
+
+  # Add and save the modified information to the database
   post '/pets/:id' do
-    @pet = Pet.find(params[:id])
-    @pet.update(params["pet"])
-    if !params["owner"]["name"].empty?
-      @pet.owner = Owner.create(name: params["owner"]["name"])
+    # binding.pry
+    @pet = Pet.find params[:id]
+    @pet.update(params[:pet])
+    if !params[:owner][:name].empty?
+      @pet.owner = Owner.create(params[:owner])
     end
     @pet.save
-    redirect "pets/#{@pet.id}"
-  end
-
-  get '/pets/:id/edit' do
-    @pet = Pet.find(params[:id])
-    erb :'/pets/edit'
+    redirect to "pets/#{@pet.id}"
   end
 end
